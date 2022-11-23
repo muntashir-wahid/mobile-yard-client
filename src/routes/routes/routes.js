d419d98e5f61e3e8da4cf92c3bda2bc29b245958
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-11-24 02:25:40 +0600

    created navbar and added some more routes

--- src/routes/routes/routes.js
+++ src/routes/routes/routes.js
@@ -1,14 +1,20 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
+import Blog from "../../Pages/Blog/Blog";
 import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
     errorElement: <ErrorPage />,
-    children: [{ index: true, element: <Home /> }],
+    children: [
+      { index: true, element: <Home /> },
+      { path: "/login", element: <Login /> },
+      { path: "/blog", element: <Blog /> },
+    ],
   },
 ]);
 
