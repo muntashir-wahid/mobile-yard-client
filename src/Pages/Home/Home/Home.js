3c0c2cd564373f1685ff6de09030d0e058a68fa0
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-11-24 12:22:59 +0600

    created banner for home page

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Hello from the home page</h1>
-    </div>
+    <header>
+      <Banner />
+    </header>
   );
 };
 
