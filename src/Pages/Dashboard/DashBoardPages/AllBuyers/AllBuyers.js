075b0c0efac7f666baa2112740262a633e7515fe
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-11-27 14:40:12 +0600

    created tabels of all seller and buyers in admin route

--- src/Pages/Dashboard/DashBoardPages/AllBuyers/AllBuyers.js
+++ src/Pages/Dashboard/DashBoardPages/AllBuyers/AllBuyers.js
@@ -1,11 +1,63 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import Loader from "../../../../components/Loader/Loader";
+import SecondaryHeading from "../../../../components/SectionHeadings/SecondaryHeading";
 
 const AllBuyers = () => {
-  return (
-    <div>
-      <h1>Hello from all buyers</h1>
-    </div>
-  );
+  const { isLoading, data } = useQuery({
+    queryKey: ["allBuyers"],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/api/v1/users?accountType=user`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <Loader
+        className="min-h-screen"
+        message="Please Wait!All buyers are loading..."
+      />
+    );
+  } else {
+    return (
+      <div className="px-4 py-10">
+        <SecondaryHeading>
+          All the buyers {data?.data?.user?.length}
+        </SecondaryHeading>
+
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            {/* <!-- head --> */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Delete Buyer</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* <!-- row 1 --> */}
+              {data?.data?.users?.map((user, index) => (
+                <tr key={user._id}>
+                  <th>{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    {<button className="btn btn-sm btn-error">Delete</button>}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default AllBuyers;
--- src/Pages/Dashboard/DashBoardPages/AllSellers/AllSellers.js
+++ src/Pages/Dashboard/DashBoardPages/AllSellers/AllSellers.js
@@ -1,11 +1,73 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
+import Loader from "../../../../components/Loader/Loader";
+import SecondaryHeading from "../../../../components/SectionHeadings/SecondaryHeading";
 
 const AllSellers = () => {
-  return (
-    <div>
-      <h1>Hello from all sellers</h1>
-    </div>
-  );
+  const { isLoading, data } = useQuery({
+    queryKey: ["allBuyers"],
+    queryFn: async () => {
+      const res = await fetch(
+        `http://localhost:5000/api/v1/users?accountType=seller`
+      );
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return (
+      <Loader
+        className="min-h-screen"
+        message="Please Wait!All sellers are loading..."
+      />
+    );
+  } else {
+    return (
+      <div className="px-4 py-10">
+        <SecondaryHeading>
+          All the buyers {data?.data?.user?.length}
+        </SecondaryHeading>
+
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            {/* <!-- head --> */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Verify Seller</th>
+                <th>Delete Buyer</th>
+              </tr>
+            </thead>
+            <tbody>
+              {/* <!-- row 1 --> */}
+              {data?.data?.users?.map((user, index) => (
+                <tr key={user._id}>
+                  <th>{index + 1}</th>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                  <td>
+                    {user?.isVerified ? (
+                      <span className="badge badge-success">Verified</span>
+                    ) : (
+                      <button className="btn btn-sm btn-secondary">
+                        Veerify Seller
+                      </button>
+                    )}
+                  </td>
+                  <td>
+                    {<button className="btn btn-sm btn-error">Delete</button>}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 };
 
 export default AllSellers;
